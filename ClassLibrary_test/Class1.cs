85098384a0fba197533f3b91c194f39ac8e9b733
DESKTOP-B65V3QH\Admin <nithesh.k@honeywell.com>
2022-07-28 13:12:17 +0530

    Add project files.

---
+++ ClassLibrary_test/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClassLibrary_test
+{
+    public class Demo
+    {
+        int i;
+        public int sum(int a, int b)
+        {
+            i = a + b;
+            return i;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("I am great");
+        }
+
+    }
+}
---
+++ ConsoleApp_new/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary_test;
+namespace ClassTest
+{
+    class ConsoleTest
+    {
+        static void Main(string[] args)
+        {
+            Demo test = new Demo();
+            test.Display();
+            
+        }
+    }
+}
